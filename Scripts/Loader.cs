bc9540f3ecc3c4f210f3d0129b827adba8e1a49e
Ernestohh <ernestohh97@gmail.com>
2016-03-16 00:19:26 +0100

    Added files via upload

---
+++ Scripts/Loader.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class Loader : MonoBehaviour
+{
+    public GameObject ply;
+
+    void Update()
+    {
+        loadScene();
+    }
+
+    public void loadScene()
+    {
+        if(ply.GetComponent<Player>().health <= 0)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }   
+    }
+
+}
