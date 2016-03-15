16f0583db841fefd0c105dc0fee07c843aa5a977
Ernestohh <ernestohh97@gmail.com>
2016-03-16 00:20:19 +0100

    Update Loader.cs

--- Scripts/Loader.cs
+++ Scripts/Loader.cs
@@ -1,9 +1,14 @@
-﻿using UnityEngine;
+using UnityEngine;
 using UnityEngine.SceneManagement;
 using System.Collections;
 
 public class Loader : MonoBehaviour
 {
+    /*
+    Version: 0.1
+    Changlog: Reload On Death
+    Ernesto
+*/
     public GameObject ply;
 
     void Update()
